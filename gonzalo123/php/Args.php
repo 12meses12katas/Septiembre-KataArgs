0eeb4b31ace3f097d35f602256683dd0325fa182
Gonzalo Ayuso <gonzalo123@gmail.com>
2011-09-19 16:02:14 +0200

    PHP. With lists and negative values

--- gonzalo123/php/Args.php
+++ gonzalo123/php/Args.php
@@ -1,14 +1,82 @@
 <?php
 class Args
 {
+    const BOOLEAN = 0;
+    const STRING  = 1;
+    const INTEGER = 2;
+    const ARRAY_STRING = 3;
+    const ARRAY_NUMBER = 4;
+
+    private $schema = array();
+    public function __construct($schema)
+    {
+        $filters = $this->initFilters();
+        foreach ($schema as $key => $type) {
+            $this->schema[$key] = $filters[$type];
+        }
+    }
+
+    private function initFilters()
+    {
+        return array(
+            self::BOOLEAN => array('filter' => FILTER_VALIDATE_BOOLEAN),
+            self::STRING => array('filter' => FILTER_SANITIZE_STRING),
+            self::INTEGER => array('filter' => FILTER_SANITIZE_NUMBER_INT),
+            self::ARRAY_STRING => array('filter' => FILTER_SANITIZE_STRING, 'flags' => FILTER_FORCE_ARRAY),
+            self::ARRAY_NUMBER => array('filter' => FILTER_VALIDATE_INT, 'flags' => FILTER_FORCE_ARRAY),
+        );
+    }
+
     public function decode($input)
     {
-        if ($input == '-l') {
-            $output = array('l' => true);
-        } elseif ($input == '-p') {
-            $output = array('p' => true);
+        $arr = explode(' ', $input);
+        $decodedInput = $output = array();
+        for ($i = 0; $i < count($arr); $i++) {
+            $nextElement = $i+1;
+            $key = substr($arr[$i], 1);
+
+            if ($this->parameterExistsInSchema($key)) {
+                if ($this->parameterHasValue($arr, $nextElement)) {
+                    if ($this->parameterIsArray($key)) {
+                        $value = explode(',', $arr[$nextElement]);
+                    } else {
+                        $value = $arr[$nextElement];
+                    }
+                    $i++;
+                } else {
+                   $value = true;
+                }
+            } else {
+                throw new Exception("Parameter {$key} not defined");
+            }
+            
+            $decodedInput[$key] = $value;
         }
 
-        return $output;
+        return filter_var_array($decodedInput, $this->schema);
+    }
+
+    private function parameterIsArray($key)
+    {
+        return isset($this->schema[$key]['flags']) && $this->schema[$key]['flags'] == FILTER_FORCE_ARRAY;
+    }
+
+    private function parameterExistsInSchema($key)
+    {
+        return array_key_exists($key, $this->schema);
+    }
+
+    private function parameterHasValue($arr, $nextElement)
+    {
+        return isset($arr[$nextElement]) && !$this->isParameter($arr[$nextElement]);
+    }
+
+    private function isParameter($parameter)
+    {
+        if (substr($parameter, 0, 1) == '-' && array_key_exists(substr($parameter, 1), $this->schema)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- gonzalo123/php/test/ArgsTest.php
+++ gonzalo123/php/test/ArgsTest.php
@@ -8,24 +8,55 @@ class ArgstTest extends PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        $this->args = new Args();
-    }
-    public function testInstalation()
-    {
-        $this->assertTrue(true);
+        $schema = array(
+            'l'  => Args::BOOLEAN,
+            'll' => Args::BOOLEAN,
+            'h'  => Args::STRING,
+            'n'  => Args::STRING,
+            'p'  => Args::INTEGER,
+            'g'  => Args::ARRAY_STRING,
+            'd'  => Args::ARRAY_NUMBER
+        );
+        $this->args = new Args($schema);
     }
 
-    public function simpleInputDataProvider()
+    public function inputDataProvider()
     {
         return array(
-            array('-l', array('l' => true)),
-            array('-p', array('p' => true))
+            'single parameter 1'  => array('-l', array('l' => true)),
+            'multiple parameter 1' => array('-l -ll', array('l' => true, 'll' => true)),
+
+            'parameter with value 1' => array('-h host', array('h' => 'host')),
+
+            'mix 1' => array('-h host -l', array('h' => 'host', 'l' => true)),
+
+            'mix 2' => array('-l -p 8080 -n /usr/logs', array(
+                'l' => true,
+                'p' => 8080,
+                'n' => '/usr/logs'
+            )),
+
+            'lists 1' => array('-g this,is,a,list -d 1,2,-3,5', array(
+                'g' => array('this', 'is', 'a', 'list'),
+                'd' => array(1, 2, -3, 5),
+            )),
+
+            'parameter with -' => array('-p -1', array('p' => -1)),
+
+            'lists 2' => array('-g this,is,a,list -p -1 -d 1,2,-3,5', array(
+                'g' => array('this', 'is', 'a', 'list'),
+                'd' => array(1, 2, -3, 5),
+                'p' => -1,
+            )),
         );
     }
 
-    /** @dataProvider simpleInputDataProvider */
-    public function testSimpleInput($input, $expected)
+    /** @dataProvider inputDataProvider */
+    public function testInput($input, $expected)
     {
-        $this->assertEquals($expected, $this->args->decode($input));
+        $actual = $this->args->decode($input);
+        foreach($expected as $key => $value) {
+            $this->assertEquals($value, $actual[$key]);
+        }
     }
 }
