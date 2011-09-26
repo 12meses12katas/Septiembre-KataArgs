3d6ce4ef36281d6036d101b7137b69351d81cd00
txingo <txingo@gmail.com>
2011-09-26 12:49:24 +0200

    Un flag booleano puede no aparecer en la lista de argumentos

--- src/main/java/es/rchavarria/kataargs/Args.groovy
+++ src/main/java/es/rchavarria/kataargs/Args.groovy
@@ -4,10 +4,24 @@ class Args {
     private schema
     private argList
     
-    def nextFlag(){
-        "b"
+    public Args(def schema, def argList){
+        this.schema = parseSchema(schema)
+        this.argList = argList
 }
+    
+    private parseSchema(def schema){
+        def flagsMap = [:]
+        def flagsList = schema.split(",")
+        flagsList.each { flagsMap[it[0]] = it[1] }
+        
+        flagsMap
+}
+
+    def nextFlag(){
+        argList[0].substring(1)
+}
+    
     def getValueOfFlag(def flag){
-        true
+        schema[nextFlag()] != null 
 }
 }
--- src/test/java/es/rchavarria/kataargs/ArgsTest.groovy
+++ src/test/java/es/rchavarria/kataargs/ArgsTest.groovy
@@ -7,12 +7,22 @@ import static org.junit.Assert.*;
 class ArgsTest {
 
     @Test
-    public void testOneBooleanFlag() {
+    public void testOneBooleanFlagTrue() {
         def schema = "bB"
         def argList = ["-b"]
-        Args args = new Args(schema:schema, argList:argList)
+        Args args = new Args(schema, argList)
         
         assert "b" == args.nextFlag()
         assert true == args.getValueOfFlag("b")
+}
+
+    @Test
+    public void testOneBooleanFlagFalse() {
+        def schema = "bB"
+        def argList = ["-f"]
+        Args args = new Args(schema, argList)
+        
+        assert "b" != args.nextFlag()
+        assert false == args.getValueOfFlag("b")
 }
 }
