ecdcb10581786553d7373965a8ae73212a1e6ab3
Funflin Splugen <funflin@gmail.com>
2011-10-07 02:28:43 +0200

    Finish list

--- funflin/php/kataArgs.php
+++ funflin/php/kataArgs.php
@@ -1,164 +1,194 @@
 <?php
-/**
-  * Class for parser comand-line arguments
-  */
+  /**
+   * Class for parser comand-line arguments
+   */
 class Args
 {
-    private $lineInput;
-    private $gen;
+  private $dataArray = array();
 
-    const INT = 0;
-    const BOOL = 1;
-    const STRING = 2;
+  const INT = 0;
+  const BOOL = 1;
+  const STRING = 2;
+  const ARRAY_STRING = 3;
+  const ARRAY_INT = 4;
 
-    private $schema = array();
-    private $defaults = array(
-            self::BOOL   => false,
-            self::INT    => '0',
-            self::STRING => '',
-    );
+  private $schema = array();
+  private $typeProperties = array(
+                                  self::BOOL   => array('default' =>false, 'empty' =>true),
+                                  self::INT    => array('default' =>'0'),
+                                  self::STRING  => array('default' =>''),
+                                  self::ARRAY_STRING => array('default' => array(''), 'array' => true),
+                                  self::ARRAY_INT => array('default' =>array('0'),'array' => true),
+                                  );
 
-    /**
-     * parser for line with arguments and data
-     *
-     * @param string $argsIn string with arguments and data
-     *
-     * @return void
-     */
-    public function parser($argsIn)
-    {
-        $this->lineInput = preg_split('/(-[a-z] *)/', $argsIn, -1, PREG_SPLIT_NO_EMPTY+PREG_SPLIT_DELIM_CAPTURE);
-        $this->generateArray();
-    }
+  /**
+   * Add new flags in schema
+   *
+   * @param string $flag character
+   * @param int    $type type constant defined in class
+   *
+   * @return void
+   */
+  public function addSchema($flag, $type)
+  {
+    $this->schema[$flag] = $type;
+  }
 
-    /**
-     * Make array with flags and values for this flags
-     *
-     * @return void
-     */
-    public function generateArray()
-    {
-        $this->gen = array();
-        $lastFlag = false;
-        foreach ($this->lineInput as $frame) {
-            if (preg_match('/(-[a-z])/', $frame)) {
-              if ($lastFlag != false) {
-                if ($this->flagInSchema($lastFlag) === true ) {
-                  $this->gen[$lastFlag] = $this->DefaultValueIfExist($lastFlag);
-                }
-              }
-              $lastFlag = $this->removeMinus($frame);
-            } else {
-                $this->gen[$lastFlag] = trim($frame);
-                $lastFlag = false;
-            }
+  /**
+   * parser for line with arguments and data
+   *
+   * @param string $argsIn string with arguments and data
+   *
+   * @return void
+   */
+  public function parser($argsIn)
+  {
+    $lineInput = preg_split('/(-[a-z] *)/', $argsIn, -1, PREG_SPLIT_NO_EMPTY+PREG_SPLIT_DELIM_CAPTURE);
+    $lastFlag = false;
+    foreach ($lineInput as $frame) {
+      if (preg_match('/(-[a-z])/', $frame)) {
+        if ($lastFlag != false) {
+          if ($this->flagInSchema($lastFlag) === true ) {
+            $this->dataArray[$lastFlag] = $this->DefaultIfEmpty($lastFlag);
+          }
         }
-    }
-
-    /**
-     * get Value for a flag give
-     *
-     * @param string $flag character or character with minus sign
-     *
-     * @return value of this flag
-     */
-    public function getValue($flag)
-    {
-      $flag = $this->removeMinus($flag);
-      $flagInSchema = $this->flagInSchema($flag);
-
-      if ($flagInSchema === true) {
-        return $this->valueForFlag($flag);
+        $lastFlag = $this->removeMinus($frame);
       } else {
-        return $flagInSchema;
+        $this->insertValue($lastFlag, $frame);
+        $lastFlag = false;
       }
     }
+  }
 
-    /**
-     * Add new flags in schema
-     *
-     * @param string $flag character
-     * @param int    $type type constant defined in class
-     *
-     * @return void
-     */
-    public function addSchema($flag, $type)
-    {
-       $this->schema[$flag] = $type;
+  /**
+   * get Value for a flag give
+   *
+   * @param string $flag character or character with minus sign
+   *
+   * @return value of this flag
+   */
+  public function getValue($flag)
+  {
+    $flag = $this->removeMinus($flag);
+    $flagInSchema = $this->flagInSchema($flag);
+
+    if ($flagInSchema === true) {
+      return $this->valueForFlag($flag);
+    } else {
+      return $flagInSchema;
     }
-    /**
-     * Return default value if exist value in schema
-     *
-     * @param string $flag character or character with minus sign
-     *
-     * @return default value
-     */
-    public function schemaDefaultValue($flag)
-    {
-      $flag = $this->removeMinus($flag);
-      return $this->defaults[$this->schema[$flag]];
+  }
+  /**
+   * Insert value in array of result
+   *
+   * @param string $flag flag to insert
+   * @param string $data with value of argument
+   *
+   * @return void
+   */
+  private function insertValue($flag, $data)
+  {
+    $data = trim($data);
+    if ($this->getProperty($flag, 'array')) {
+      $this->dataArray[$flag] = explode(',', $data);
+    } else {
+      $this->dataArray[$flag] = $data;
     }
-    /**
-     * Return the default value if exist the flag,
-     *
-     * @param string $flag character or character with minus sign
-     *
-     * @return default Value when flag exist
-     */
-    public function DefaultValueIfExist($flag)
-    {
-      $flag = $this->removeMinus($flag);
-      if ($this->schema[$flag] == self::BOOL) {
-        $return = true;
-      } else {
-        $return = $this->schemaDefaultValue($flag);
-      }
-      return $return;
+  }
+  /**
+   * find property of flag in type of data
+   *
+   * @param string $flag     character
+   * @param string $property to find
+   *
+   * @return value of property or false
+   */
+  private function getProperty($flag, $property)
+  {
+    $data = $this->typeProperties[$this->schema[$flag]];
+    if (array_key_exists($property, $data)) {
+      $value = $data[$property];
+    } else {
+      $value = false;
     }
-    /**
-     * Find the flag in schema if not exist retrun error
-     *
-     * @param string $flag character or character with minus sign
-     *
-     * @return True or string with Error
-     */
-    private function flagInSchema($flag)
-    {
-      $flag = $this->removeMinus($flag);
-      if (!array_key_exists($flag, $this->schema)) {
-        return "The $flag flag is incorrect";
-      }
-      return true;
+    return $value;
+  }
+
+  /**
+   * Return default value if exist value in schema
+   *
+   * @param string $flag character or character with minus sign
+   *
+   * @return default value
+   */
+  private function schemaDefaultValue($flag)
+  {
+    $flag = $this->removeMinus($flag);
+    return $this->getProperty($flag, 'default');
+  }
+  /**
+   * Return the default value if exist the flag,
+   *
+   * @param string $flag character or character with minus sign
+   *
+   * @return default Value when flag exist
+   */
+  private function DefaultIfEmpty($flag)
+  {
+    $flag = $this->removeMinus($flag);
+    $property = $this->getProperty($flag, 'empty');
+
+    if ($property) {
+      $return = $property;
+    } else {
+      $return = $this->schemaDefaultValue($flag);
     }
-    /**
-     * return value for a flag given or default value if not exist this
-     *
-     * @param string $flag character
-     *
-     * @return value for flag
-     */
-    private function valueForFlag($flag)
-    {
-      if (array_key_exists($flag, $this->gen)) {
-        $return = $this->gen[$flag];
-      } else {
-        $return = $this->schemaDefaultValue($flag);
-      }
-      return $return;
+    return $return;
+  }
+  /**
+   * Find the flag in schema if not exist retrun error
+   *
+   * @param string $flag character or character with minus sign
+   *
+   * @return True or string with Error
+   */
+  private function flagInSchema($flag)
+  {
+    $flag = $this->removeMinus($flag);
+    if (!array_key_exists($flag, $this->schema)) {
+      return "The $flag flag is incorrect";
     }
-    /**
-     * remove minus sign of a flag give if hava minus sign
-     *
-     * @param string $flag character or character with minus sign
-     *
-     * @return flag without minus sign
-     */
-    private function removeMinus($flag)
-    {
-      if (strlen($flag)>1) {
-        $val = explode('-', $flag);
-        $flag = trim($val[1]);
-      }
-      return $flag;
+    return true;
+  }
+  /**
+   * return value for a flag given or default value if not exist this
+   *
+   * @param string $flag character
+   *
+   * @return value for flag
+   */
+  private function valueForFlag($flag)
+  {
+    if (array_key_exists($flag, $this->dataArray)) {
+      $return = $this->dataArray[$flag];
+    } else {
+      $return = $this->schemaDefaultValue($flag);
     }
+    return $return;
+  }
+  /**
+   * remove minus sign of a flag give if hava minus sign
+   *
+   * @param string $flag character or character with minus sign
+   *
+   * @return flag without minus sign
+   */
+  private function removeMinus($flag)
+  {
+    if (strlen($flag)>1) {
+      $val = explode('-', $flag);
+      $flag = trim($val[1]);
+    }
+    return $flag;
+  }
 }
--- funflin/php/test/ArgsTest.php
+++ funflin/php/test/ArgsTest.php
@@ -29,6 +29,18 @@ class ArgsTest extends PHPUnit_Framework_TestCase
     $this->ar->addSchema('d', Args::STRING);
   }
   /**
+   * Generate a advance schema for test
+   *
+   * @return void
+   */
+  public function advanceSchema()
+  {
+    $this->ar->addSchema('l', Args::BOOL);
+    $this->ar->addSchema('p', Args::INT);
+    $this->ar->addSchema('d', Args::ARRAY_INT);
+    $this->ar->addSchema('g', Args::ARRAY_STRING);
+  }
+  /**
    * Data provaider for test
    *
    * @return void
@@ -60,6 +72,14 @@ class ArgsTest extends PHPUnit_Framework_TestCase
                                                     'd'=>'/usr/logs'
                                                     )
                                               ),
+                 "args  with duplicate values" => array(
+                                                        '-p 50 -d /usr/logs -l -p 8080',
+                                                        array(
+                                                              'l'=>true,
+                                                              'p'=>'8080',
+                                                              'd'=>'/usr/logs'
+                                                              )
+                                                        ),
                  "args with empty args" => array(
                                                  '-d -p',
                                                  array(
@@ -76,6 +96,7 @@ class ArgsTest extends PHPUnit_Framework_TestCase
                                               'd'=>''
                                               )
                                         ),
+
                  "with flag incorrect" => array(
                                                 '-d /usr/logs -l -p 8080 -w',
                                                 array(
@@ -88,6 +109,33 @@ class ArgsTest extends PHPUnit_Framework_TestCase
                  );
   }
 
+  /**
+   * Data provaider for test
+   *
+   * @return void
+   */
+  public function generateAdvanceDataTestCase()
+  {
+    return array(
+                 "args complete in order" => array(
+                                                   '-l -p 8080 -d 8,7,5,3 -g this,is,a,list',
+                                                   array(
+                                                         'l'=>true,
+                                                         'p'=>'8080',
+                                                         'd'=>array(8,7,5,3),
+                                                         'g'=>array('this','is','a','list'),
+                                                         )
+                                                   ),
+                 "args empty" => array(
+                                       '-l -d -g',
+                                       array(
+                                             'l'=>true,
+                                             'd'=>array('0'),
+                                             'g'=>array(''),
+                                             ),
+                                       ),
+                 );
+  }
   /**
    * Take data of data provider and generate test with basic schema
    *
@@ -102,14 +150,40 @@ class ArgsTest extends PHPUnit_Framework_TestCase
   public function testArgsGenerico($args, $testValue)
   {
     $this->basicSchema();
+    $this->checkValues($args, $testValue);
+  }
+
+  /**
+   * Take data of data provider and generate test with advance schema
+   *
+   * @param string $args      line of arguments to analize
+   * @param array  $testValue experated returns
+   *
+   * @return void
+   *
+   * @dataProvider generateAdvanceDataTestCase
+   *
+   */
+  public function testArgsAdvance($args, $testValue)
+  {
+    $this->advanceSchema();
+    $this->checkValues($args, $testValue);
+  }
+
+  /**
+   * make test with arguments and experated values
+   *
+   * @param string $args      line of arguments to analize
+   * @param array  $testValue experated returns
+   *
+   * @return void
+   */
+  public function checkValues($args, $testValue)
+  {
     $this->ar->parser($args);
     foreach ($testValue as $key => $test) {
       $this->assertEquals($test, $this->ar->getValue($key));
     }
   }
-
 }
-/*
- * Stage2
- * Añadir soporte para listas
- */
+
